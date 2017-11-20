f556cbc6c06e8a0718390f237595e1524eec5180
Vikesh Jhajhria <vikesh.jhajhria@gmail.com>
2017-11-20 23:06:56 +0530

    Initial commit

---
+++ app/src/main/java/com/newsapp/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.newsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Vikesh on 19-Nov-17.
+ */
+
+public class BaseActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+
+    }
+}
